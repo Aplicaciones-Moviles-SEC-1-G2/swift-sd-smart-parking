0c5702d8a3950bba29242e393d7d0b4bea89dd17
Mateo Rincon <m.rinconz@uniandes.edu.co>
2026-03-19 09:28:18 -0500

    Initial iOS project setup with Xcode

---
+++ sd-smart-parking/sd-smart-parking/sd_smart_parkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  sd_smart_parkingApp.swift
+//  sd-smart-parking
+//
+//  Created by Mateo on 19/03/26.
+//
+
+import SwiftUI
+
+@main
+struct sd_smart_parkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
