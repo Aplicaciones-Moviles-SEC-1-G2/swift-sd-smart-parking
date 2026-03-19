d6164b8466d375cb50abbc64d08a5df846e2d8aa
Mateo Rincon <m.rinconz@uniandes.edu.co>
2026-03-19 09:42:48 -0500

    .plist files and additional initial config

--- sd-smart-parking/sd-smart-parking/sd_smart_parkingApp.swift
+++ sd-smart-parking/sd-smart-parking/sd_smart_parkingApp.swift
@@ -7,11 +7,28 @@
 
 import SwiftUI
 
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
 @main
 struct sd_smart_parkingApp: App {
+    
+    @StateObject private var navigationManager = NavigationManager()
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(navigationManager)
         }
     }
 }
+
+
